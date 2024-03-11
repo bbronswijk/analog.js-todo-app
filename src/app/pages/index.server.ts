3b0e1ba473c636f5d5f67a71f2c387b14ffde047
bbronswijk <brambronswijk@gmail.com>
2024-03-11 21:57:28 +0100

    Return todos by order of createdAt

--- src/app/pages/index.server.ts
+++ src/app/pages/index.server.ts
@@ -2,5 +2,7 @@ import { prisma } from '../../server/prisma-client';
 import { Todo } from '@prisma/client';
 
 export const load = async (): Promise<{ todos: Todo[] }> => ({
-  todos: await prisma.todo.findMany()
+  todos: await prisma.todo.findMany({
+    orderBy: { createdAt: 'desc' },
+  })
 });
